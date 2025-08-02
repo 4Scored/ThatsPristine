dab628681f9f67fd245a4c7dfb6e2a776e5cdb62
John <johnzionwong33@gmail.com>
2025-08-01 22:18:42 -0700

    remove clean_text debug comments

--- cleaner/text_handler.py
+++ cleaner/text_handler.py
@@ -7,9 +7,6 @@ def clean_text(df, normalize=False, remove_punc=False):
         df[col] = df[col].astype(str).str.strip()
         if normalize:
             df[col] = df[col].astype(str).str.lower()
-        if remove_punc:
-            print("CHECK")
-            df[col] = df[col].astype(str).str.translate(str.maketrans('', '', string.punctuation))   
-            print(df[col])
-            print("CHECK")
+        if remove_punc:            
+            df[col] = df[col].astype(str).str.translate(str.maketrans('', '', string.punctuation))               
     return df
