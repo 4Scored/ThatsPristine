b38e17f51363ff6abc03d6805c32cc637b1b5406
John <johnzionwong33@gmail.com>
2025-08-02 10:20:19 -0700

    implement clean_column_names; modify clean_text_data (regex)

--- cleaner/text_handler.py
+++ cleaner/text_handler.py
@@ -1,12 +1,18 @@
+import re
 import string
 
-def clean_text(df, normalize=False, remove_punc=False):
+def clean_text_data(df, normalize=False, remove_punc=False):
     df = df.copy()
     obj_cols = df.select_dtypes(include="object")
     for col in obj_cols:
         df[col] = df[col].astype(str).str.strip()
-        if normalize:
-            df[col] = df[col].astype(str).str.lower()
-        if remove_punc:            
-            df[col] = df[col].astype(str).str.translate(str.maketrans('', '', string.punctuation))               
+        if remove_punc: df[col] = df[col].astype(str).str.translate(str.maketrans('', '', string.punctuation)) 
+        if normalize: df[col] = df[col].astype(str).str.lower().str.replace(r'\s+', ' ', regex=True)        
+    return df
+
+def clean_column_names(df, normalize=False, remove_punc=False):
+    df = df.copy()    
+    df.columns = df.columns.str.strip()     
+    if remove_punc: df.columns = df.columns.astype(str).str.translate(str.maketrans('', '', string.punctuation)) 
+    if normalize: df.columns = df.columns.astype(str).str.lower().str.replace(r'\s+', '_', regex=True)  
     return df
