65aab5b91455d6d6fabc4571e67f6254f14982d6
John <johnzionwong33@gmail.com>
2025-08-09 16:33:02 -0700

    reformat type_handler

--- cleaner/type_handler.py
+++ cleaner/type_handler.py
@@ -1,17 +1,41 @@
 import pandas as pd
 from dateutil import parser
 
+BOOL_VALS_MAP = {
+    "true" : True, "false" : False, 
+    "t" : True, "f" : False, 
+    "1" : True, "0" : False,
+    "yes" : True, "no" : False,
+}
+
 def convert_types(df, bool_involved=True, inplace=False):
     if not inplace:
         df = df.copy() 
 
     for col in df.columns:
+        if pd.api.types.is_numeric_dtype(df[col]) \
+        or pd.api.types.is_datetime64_any_dtype(df[col]) \
+        or pd.api.types.is_bool_dtype(df[col]):
+            continue
+            
         converted = False
 
         # Numeric Conversion    
         try:
             df[col] = pd.to_numeric(df[col])
             converted = True
+        except:
+            pass
+
+        # Boolean Conversion
+        try:
+            if bool_involved and not converted:
+                valid_bool_vals = set(BOOL_VALS_MAP.keys())
+                
+                lowercased_col_bool_vals = df[col].dropna().astype(str).str.strip().str.lower()                                
+                if (set(lowercased_col_bool_vals.unique())).issubset(valid_bool_vals):
+                    df[col] = lowercased_col_bool_vals.map(BOOL_VALS_MAP).astype(bool)
+                    converted = True                    
         except:
             pass
 
@@ -28,24 +52,6 @@ def convert_types(df, bool_involved=True, inplace=False):
                 if parsed.notna().sum() >= 0.2 * len(parsed):
                     df[col] = parsed
                     converted = True
-        except:
-            pass
-
-        # Boolean Conversion
-        try:
-            if bool_involved and not converted:
-                bool_like_vals = {"true" : True, "false" : False, 
-                                  "t" : True, "f" : False, 
-                                  "1" : True, "0" : False,
-                                  "yes" : True, "no" : False,
-                                  }
-                valid_bool_vals = set(bool_like_vals.keys())
-                
-                lowercased_col_bool_vals = df[col].dropna().astype(str).str.strip().str.lower()                                
-                if (set(lowercased_col_bool_vals.unique())).issubset(valid_bool_vals):
-                    df[col] = lowercased_col_bool_vals.map(bool_like_vals).astype(bool)
-                    converted = True
-                    
         except:
             pass
 
